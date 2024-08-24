e4a84b4b522909059555536f1723c45420da2213
Abhinav Kasam <abhinavkl1412.gmail.com>
2024-08-24 23:14:14 +0530

    added web app to call the api, added OpenIDConnect authentication.

---
+++ CustomerApplication/Models/Customer Data/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomerApplication.Models.Customer_Data
+{
+    public partial class Customer
+    {
+        public string _id { get; set; }
+		public string Id { get; set; }
+		
+		public string Type { get; set; }
+
+		public string CustomerId { get; set; }
+		public string Title { get; set; }
+		public string FirstName { get; set; }
+        public string LastName { get; set; }
+		public string EmailAddress { get; set; }
+		public string PhoneNumber { get; set; }
+		public DateTime CreationDate { get; set; }
+		public List<Address> Addresses { get; set; }
+		public int SalesOrderCount { get; set; }
+    }
+}
+
